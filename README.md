8e959dc08852108cfd3b174364181ef670c3a2b9
Jegor <egorlins@hotmail.com>
2022-09-24 19:42:48 +0300

    changed integrattion test to work with containers

---
+++ README.md
@@ -0,0 +1,20 @@
+# spring-data-jpa
+
+https://www.jetbrains.com/idea/download/#section=windows
+https://www.mysql.com/products/workbench/
+https://dev.mysql.com/downloads/installer/
+
+Create Spring project
+https://start.spring.io/
+
+add containers
+https://www.testcontainers.org/quickstart/junit_5_quickstart/
+
+https://www.testcontainers.org/modules/databases/mysql/
+
+
+docker: search for docker hub
+
+https://hub.docker.com/ -> search for mysql "mysql docker oficial image" 
+
+https://hub.docker.com/_/mysql and check tags versions
--- src/test/java/com/egorlins/springboot/SpringbootTestcontainersDemoApplicationTests.java
+++ src/test/java/com/egorlins/springboot/SpringbootTestcontainersDemoApplicationTests.java
@@ -10,15 +10,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Testcontainers
 class SpringbootTestcontainersDemoApplicationTests {
 
+	//NB it should be static, otherwise it will start and stop before and after every testcase not the whole test class
+	@Container
+	private static MySQLContainer mySQLContainer = new MySQLContainer("mysql:latest");
 	@Autowired
 	private StudentRepository studentRepository;
 
@@ -28,6 +34,11 @@ class SpringbootTestcontainersDemoApplicationTests {
 	// given/when/then format - BDD style
 	@Test
 	public void givenStudents_whenGetAllStudents_thenListOfStudents() throws Exception {
+
+		System.out.println(mySQLContainer.getDatabaseName());
+		System.out.println(mySQLContainer.getPassword());
+		System.out.println(mySQLContainer.getUsername());
+		System.out.println(mySQLContainer.getJdbcUrl());
 		//given - setup or precondition
 		List<Student> students = List.of(
 				Student.builder().firstName("John").lastName("Smith").email("john@gmail.com").build(),
